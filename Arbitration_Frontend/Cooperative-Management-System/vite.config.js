578f3cdb00b9456d2b69862211e127c551605c09
nnuwansa <nadininuwansa@gmail.com>
2026-05-08 11:19:14 +0530

    Updated

--- Arbitration_Frontend/Cooperative-Management-System/vite.config.js
+++ Arbitration_Frontend/Cooperative-Management-System/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            // node_modules athule thiyena libraries wenama files walata kadanawa
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+    // Warning eka ena limit eka 1000kb wenakan wedi karanna
+    chunkSizeWarningLimit: 1000,
+  },
 })
